ea41c81b85d816441393633cdba87eb5672afd19
Callum Thickett <cthickett99@hotmail.com>
2024-01-20 13:39:51 +0000

    re-factor: update href in navbar

--- src/components/HiddenNavBar.tsx
+++ src/components/HiddenNavBar.tsx
@@ -46,16 +46,16 @@ function HiddenNavBar() {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/Projects">
                   Projects
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/Demos">
                   Demos
                 </a>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="/CV">
                     CV
                   </a>
                 </li>

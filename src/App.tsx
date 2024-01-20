612bf6672f0524573502e923c6b967cf5683cb01
Callum Thickett <cthickett99@hotmail.com>
2024-01-20 20:14:23 +0000

    refactor: use routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/homePage";
-import ArticlesPage from "./components/articlesPage";
-import ProjectsPage from "./components/projectsPage";
-import DemosPage from "./components/demosPage";
-import CvPage from "./components/cvPage";
+import routesConfig from "./configs/routesConfig";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Articles" element={<ArticlesPage />} />
-        <Route path="/Projects" element={<ProjectsPage />} />
-        <Route path="/Demos" element={<DemosPage />} />
-        <Route path="/CV" element={<CvPage />}></Route>
+        {routesConfig.map((item) => (
+          <Route path={item.path} element={<item.component />}></Route>
+        ))}
       </Routes>
     </Router>
   );

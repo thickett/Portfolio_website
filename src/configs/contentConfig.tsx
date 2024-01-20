280c13685134b2920594d36150c081df61ca811a
Callum Thickett <cthickett99@hotmail.com>
2024-01-20 22:01:36 +0000

    build: add config file for external content
    - simple framework to add articles, github repositories etc

---
+++ src/configs/contentConfig.tsx
@@ -0,0 +1,13 @@
+const mediumContent = [
+  {
+    article_name:
+      "Effortless Insights: Your Guide to Creating LLM-Based AI for Advanced Database Queries and Analysis",
+    article_content:
+      "In the ever-evolving landscape of data analysis, a challenge often lies in translating complex data queries into clear, actionable insights. Consider a scenario where a business needs to understand how a particular subset of users are interacting with one of their products. Traditionally, this would involve navigating through intricate database schemas and writing detailed SQL queries — a daunting task for many product-oriented professionals, and thus a dependency is created on the data teams.",
+    article_link: "https://medium.com/p/a9ffdec14025",
+    article_image_src: "src/assets/medium_article_image_one.webp",
+    article_image_alt:
+      "A medium article I wrote about how to build a conversational chatbot that can interact directly with databases and perform analysis.",
+    publish_date: "2024-01-18",
+  },
+];

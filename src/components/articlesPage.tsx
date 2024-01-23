b18dff88b1a9d0d0acc24521f91cce3e627522eb
Callum Thickett <cthickett99@hotmail.com>
2024-01-23 22:28:23 +0000

    feat: add title and content to articles

--- src/components/articlesPage.tsx
+++ src/components/articlesPage.tsx
@@ -1,6 +1,20 @@
-import GenericLayout from "./GenericLayout";
+import GenericLayoutBackPages from "./GenericLayoutBackPages";
+import GridCards from "./gridCards";
+import { mediumContent } from "../configs/contentConfig";
+import { Container } from "react-bootstrap";
 
 function articlesPage() {
-  return <GenericLayout pageTitle="">{}</GenericLayout>;
+  return (
+    <GenericLayoutBackPages
+      pageTitle="Articles"
+      pageSubTitle="From time to time I write articles on Medium outlining my recent work and thoughts. I also have some tutorial-esk articles."
+    >
+      <Container>
+        <div>
+          <GridCards all_content={mediumContent}></GridCards>
+        </div>
+      </Container>
+    </GenericLayoutBackPages>
+  );
 }
 export default articlesPage;

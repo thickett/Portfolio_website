d6f131a3deea4ba64507ddf78aa7fd0093c1b7af
Callum Thickett <cthickett99@hotmail.com>
2024-01-23 22:29:41 +0000

    style: change some default styles
    - change the way pageTitle looks
    -render an optional pagesubtitle

--- src/components/GenericLayout.tsx
+++ src/components/GenericLayout.tsx
@@ -1,22 +1,23 @@
 import { ReactNode } from "react";
 import HiddenNavBar from "./HiddenNavBar";
 import "./styles/PageLayout.css";
+import "./styles/pageTitle.css";
 
 function GenericLayout({
   pageTitle,
+  pageSubTitle,
   children,
 }: {
   pageTitle: string;
+  pageSubTitle?: string;
   children: ReactNode;
 }) {
   return (
     <>
       <HiddenNavBar />
-      <div
-        className="pageTitle"
-        style={{ textAlign: "center", marginTop: "60px" }}
-      >
-        <p> {pageTitle}</p>
+      <div className="horizontal-line-bottom center-text-margin ">
+        <p className="title-text-large">{pageTitle}</p>
+        {pageSubTitle && <p className="title-text-sub">{pageSubTitle}</p>}
       </div>
       <main>{children}</main>
     </>
